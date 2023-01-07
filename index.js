70cb4ac553054779c37b55a663eb37416d451b3d
atish nishant <atish.nishant@gmail.com>
2023-01-07 09:55:14 +0530

    create views & view-engine--done

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port=8000;
 const app=express();
 //defining router for all routes to 'routes' folder using middleware
 app.use('/',require('./routes'));
+//setting up view-engine & views directory
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log(`Error : ${err}`);
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ const userController=require('../controller/user_controller');
 const router=express.Router();
 //setting routes & controller of '/users'
 router.get('/',function(req,res){       
-    res.send('<h1>User page</h1>');
+    return res.render('user',{
+        title:'User page'
+    });
 });
 //setting routes & controller of '/users/profiles'
 router.get('/profile',userController.profile);
