c2163470df7b5b93b9ef35addcbfd8f2dad41049
atish nishant <atish.nishant@gmail.com>
2023-01-08 10:21:22 +0530

    create partial,layout & static files --done

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const port=8000;
 const app=express();
+const expressLayout=require('express-ejs-layouts');
+//defining a layout
+app.use(expressLayout);
+//access to static pages for layout
+app.use(express.static('./assets'));
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //defining router for all routes to 'routes' folder using middleware
 app.use('/',require('./routes'));
 //setting up view-engine & views directory
