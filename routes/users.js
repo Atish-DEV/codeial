e4287c02c5e6a9de50502f6f2386233b54b6fff8
atish nishant <atish.nishant@gmail.com>
2023-01-09 12:31:52 +0530

    create sign-in & sign-up form--done

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,10 @@ router.get('/',function(req,res){
 });
 //setting routes & controller of '/users/profiles'
 router.get('/profile',userController.profile);
+//setting routes & controller of '/users/sign-up'
 router.get('/sign-up',userController.signUp);
+//setting routes & controller of '/users/sign-in'
 router.get('/sign-in',userController.signIn);
+//setting routes & controller of '/users/create-user'
+router.post('/create-user',userController.createUser);
 module.exports=router;
