b68cdbe80be3865d1659dad34b14c9115eb59436
atish nishant <atish.nishant@gmail.com>
2023-01-17 17:35:16 +0530

    comment delete --done

--- routes/comments.js
+++ routes/comments.js
@@ -3,4 +3,5 @@ const passport=require('passport');
 const commentController=require('../controller/comment_controller');
 const router=express.Router();
 router.post('/create-comment',passport.checkAuthentication,commentController.createComment);
+router.get('/destroy/:id',passport.checkAuthentication,commentController.destroy);
 module.exports=router;
